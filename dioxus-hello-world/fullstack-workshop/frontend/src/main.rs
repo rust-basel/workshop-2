f889194b8d002a21a8cb26a3ac1b3bc2e74b18ae
yguenduez <yasinguenduez1@gmail.com>
2024-04-07 21:35:52 +0200

    add all the styling

--- dioxus-hello-world/fullstack-workshop/frontend/src/main.rs
+++ dioxus-hello-world/fullstack-workshop/frontend/src/main.rs
@@ -7,5 +7,13 @@ fn main() {
 #[allow(non_snake_case)]
 pub fn App() -> Element {
     let rust_basel = "Rust Basel";
-    rsx! {"Welcome to {rust_basel}"}
+    rsx! {
+        h1{
+            "Welcome to {rust_basel}"
+        }
+        button{
+            class: "btn",
+            "My stylish button"
+        }
+    }
 }
