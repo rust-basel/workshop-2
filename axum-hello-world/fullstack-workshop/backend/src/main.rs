bf679b5e315625c2f211f692b5d2138ef452581b
yguenduez <yasinguenduez1@gmail.com>
2024-04-08 21:31:33 +0200

    add route with json echo

--- axum-hello-world/fullstack-workshop/backend/src/main.rs
+++ axum-hello-world/fullstack-workshop/backend/src/main.rs
@@ -1,4 +1,9 @@
-use axum::{extract::Path, response::IntoResponse, routing::get, Router};
+use axum::{
+    extract::Path,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
 
 async fn hello_world() -> impl IntoResponse {
     "Hello World"
@@ -8,13 +13,25 @@ async fn hello_name(Path(name): Path<String>) -> impl IntoResponse {
     format!("Hello {name}")
 }
 
-
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(hello_world))
-        .route("/:name", get(hello_name));
+        .route("/:name", get(hello_name))
+        .route("/your-route", post(workshop_echo));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3001").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Workshop {
+    attendees_count: i32,
+    people_like_it: bool,
+}
+
+async fn workshop_echo(Json(workshop): Json<Workshop>) -> impl IntoResponse {
+    Json(workshop)
 }
