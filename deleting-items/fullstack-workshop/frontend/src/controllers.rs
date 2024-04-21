d0c17eb2e1366eb069b525877aa8112cbc24c939
yguenduez <yasinguenduez1@gmail.com>
2024-04-21 08:46:25 +0200

    add deleting helper fn

--- deleting-items/fullstack-workshop/frontend/src/controllers.rs
+++ deleting-items/fullstack-workshop/frontend/src/controllers.rs
@@ -21,3 +21,12 @@ pub async fn post_item(item: PostShopItem) -> Result<ShoppingListItem, reqwest::
 
     Ok(response)
 }
+
+pub async fn delete_item(item_id: &str) -> Result<(), reqwest::Error> {
+    reqwest::Client::new()
+        .delete(format!("http://localhost:3001/items/{}", item_id))
+        .send()
+        .await?;
+
+    Ok(())
+}
